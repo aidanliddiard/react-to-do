cea61a84655955bcdf83b259e8292d27294bcd95
aidanliddiard <aidanliddiard@gmail.com>
2022-04-01 10:26:02 -0700

    adding todo working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import ListPage from './ Views/ListPage';
 import Home from './ Views/Home';
+import AddPage from './ Views/AddPage';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route exact path="/list">
             <ListPage />
+          </Route>
+          <Route exact path="/add-todo">
+            <AddPage />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/services/todo.js
+++ src/services/todo.js
@@ -4,3 +4,9 @@ export async function fetchToDos() {
   const resp = await client.from('todos').select('*');
   return checkError(resp);
 }
+
+export async function addToDo(todo) {
+  console.log(todo);
+  const resp = await client.from('todos').insert({ todo: todo, complete: false });
+  return checkError(resp);
+}
